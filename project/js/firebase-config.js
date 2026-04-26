61f5c77817a5ba4d9c1a8c3a2d65488c15e857d2
somyajha1233 <145524057+somyajha1233@users.noreply.github.com>
2026-04-26 21:19:46 +0545

    update

---
+++ project/js/firebase-config.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDidUOtm9aish8IIOSh3YzY0iDSUYvSJK0",
+    authDomain: "notesappdemo-f659d.firebaseapp.com",
+    projectId: "notesappdemo-f659d",
+    storageBucket: "notesappdemo-f659d.firebasestorage.app",
+    messagingSenderId: "495307596121",
+    appId: "1:495307596121:web:1922f293d8e87d0cf9ec48"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
